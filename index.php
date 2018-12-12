369ff768631adfb9b74aead39e84b637d7fdbdf8
藤之内 <tengzhinei@tengzhineideMacBook-Pro.local>
2018-12-13 01:47:09 +0800

    d

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 define('IS_CLI', PHP_SAPI == 'cli' ? true : false);
 define('IS_WIN', strpos(PHP_OS, 'WIN') !== false);
-defined('IS_SWOOLE') or define('IS_SWOOLE', IS_CLI&&$argv[1]=='http'||$argv[1]=='websocket');
+define('IS_SWOOLE', IS_CLI&&$argv[1]=='http'||$argv[1]=='websocket');
 define('IS_DEV',false);
 define('ROOT_PATH',__DIR__.DIRECTORY_SEPARATOR);
 require __DIR__ . '/vendor/duohuo/rapphp/start.php';
